d2d4f02eb2a76f1d29d530d10c5945c60b0b5b97
Vinicius Figueiredo <vinifig@hotmail.com>
2016-10-19 14:00:50 -0200

    Bug fix

--- Ionic/www/js/App/controller.js
+++ Ionic/www/js/App/controller.js
@@ -8,7 +8,22 @@
         const updateUsers = function(){
           UserService.getAll().then(
             (users) => {
-              $scope.users = users;
+              users.forEach(function(user){
+                let found = false;
+                for(let i = 0; i < $scope.users.length; i++){
+                  if($scope.users[i]._id == user._id){
+                    found = true;
+                    for(let key in user){
+                      if($scope.users[i][key] != user[key]);
+                      $scope.users[i][key] = user[key]
+                    }
+                    break;
+                  }
+                }
+                if(!found){
+                  $scope.users.push(user);
+                }
+              })
             },
             (data, status) => {
               console.log(`error: ${status}`);
